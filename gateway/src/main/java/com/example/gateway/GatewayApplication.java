c0904454f92c132bec1a24db70fae58dbb638975
zl <zhaluobox@163.com>
2022-06-29 11:14:09 +0800

    no message

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created  on 2022/6/29 10:10:07
+ *
+ * @author zl
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
